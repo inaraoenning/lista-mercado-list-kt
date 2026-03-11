a15557a93b325036b63bebe547135cb84e47deaf
Inara <inaraoenning@gmail.com>
2026-03-11 17:58:16 -0300

    Add funcoes lista

---
+++ src/listaList.kt
@@ -0,0 +1,68 @@
+fun main() {
+
+    // Criar uma lista para nome
+    // Criar uma lista para valor
+
+    // while(true) menu
+    // when para menu opções
+
+    // adicionar -> verifica se existe o item com o nome
+    // remover -> verifica se existe antes de remover
+    // alterar valor -> verificar se existe e alterar o valor onde o indice é igual ao nome do item
+    // ver lista -> usar for in percorenddo o tamanho inteiro da lista e printar nome e valor
+    // sair
+
+    // Declara lista vazia
+    val nomeItem = mutableListOf<String>()
+    val valorItem = mutableListOf<Double>()
+
+    while (true) {
+        println("=== Lista de Compras ===")
+        print(
+            """
+            1- Adicionar
+            2- Remover
+            3- Alterar valor
+            4- Ver lista
+            5- Sair
+        """.trimIndent()
+        )
+        val opcao: Int = readln().toInt()
+
+        when (opcao) {
+            1 -> {
+                println("Informe o nome do produto:")
+                val nome: String = readln()
+
+                // Verifica se o item já existe antes de passar o valor
+                if (nomeItem.contains(nome)) {
+                    println("🟡 Item já existe na lista.")
+                    continue
+                }
+                print("Informe o valor do produto:")
+                val valor: Double = readln().toDouble()
+
+                nomeItem.add(nome)
+                valorItem.add(valor)
+
+                println("$nome foi adicionar a lista.")
+
+            }
+            2-> {
+                println("Informe o nome do produto que deseja remover:")
+                val nome: String = readln()
+
+                // Verifica se o item existe na lista para remover
+                if(!nomeItem.contains(nome)){
+                    print("Item não encontrado.")
+                    continue
+                }
+
+                // val busca = nomeItem.find { it.contains(nome) }
+
+                //nomeItem.removeAt(nome)
+            }
+        }
+    }
+}
+
